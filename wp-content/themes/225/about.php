3b5f1966015c19413fe26b457e1fc20baf057f4e
Andrew Levy <andrewlevy@andrew.local>
2018-07-24 14:19:00 -0400

    work

--- wp-content/themes/225/about.php
+++ wp-content/themes/225/about.php
@@ -3,19 +3,30 @@
 <?php get_header(); ?>
 <div class="row" id="aboutTop">
   <div class="col-md-4">
-    <h1>Our Data Advantage</h1>
+    <h1>OUR DATA ADVANTAGE</h1>
     <p>Our relationship with Venus Concept gives our advertising the most accurate targeting in the industry. We leverage non-identifiable patient data to optimize digital ad campaigns in real time. Simply put, we are the best-equipped to deliver the right ads to the right audience at the right time for your practice.</p>
   </div>
   <div class="col-md-8">
-    <img src="<?php echo get_bloginfo( 'template_directory' );?>/images/data_graphic.png" alt="">
+    <img src="<?php echo get_bloginfo( 'template_directory' );?>/images/about/data_graphic.png" alt="">
   </div>
 </div>
 <div class="row" id="aboutContent">
-  <div class="col-sm-12 about-content-image">
-    <div class="row">
-      <div class="col-sm-4 offset-sm-4">
-        <h2>The Team</h2>
+  <hr>
+  <div class="col-sm-12 about-content-bg">
+    <div class="row content-row">
+      <div class="col-sm-10 offset-sm-1 col-md-8 offset-md-2 about-content-text">
+        <h2>THE TEAM</h2>
         <p>We hand-picked our team from thje most successful branding and media buying agnecies of the digital era. We add our experience working for brands like CocaCola, The New York Jets, and Mercedez Benz to the decades of medical aesthetic knowledge within Venus Concept.</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-10 offset-md-1 col-sm-12">
+        <img class="staff-image" src="<?php echo get_bloginfo( 'template_directory' );?>/images/about/About-Page_Isolated_Image.png" alt="">
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <img class="icons-image" src="<?php echo get_bloginfo( 'template_directory' );?>/images/about/About_Icons_TEXT.png" alt="">
       </div>
     </div>
   </div>

c7d2c54313df6c9b6b1bc3f247b81bd3fd6b718e
Andrew Levy <andrewlevy@andrew.local>
2018-07-25 10:11:43 -0400

    home and about wrap up

--- wp-content/themes/225/about.php
+++ wp-content/themes/225/about.php
@@ -11,22 +11,31 @@
   </div>
 </div>
 <div class="row" id="aboutContent">
-  <hr>
+  <img class="gold-bar" src="<?php echo get_bloginfo( 'template_directory' );?>/images/homepage-header_2_gold_bar.jpg" alt="">
   <div class="col-sm-12 about-content-bg">
     <div class="row content-row">
       <div class="col-sm-10 offset-sm-1 col-md-8 offset-md-2 about-content-text">
-        <h2>THE TEAM</h2>
+        <h1>THE TEAM</h1>
         <p>We hand-picked our team from thje most successful branding and media buying agnecies of the digital era. We add our experience working for brands like CocaCola, The New York Jets, and Mercedez Benz to the decades of medical aesthetic knowledge within Venus Concept.</p>
       </div>
     </div>
     <div class="row">
-      <div class="col-md-10 offset-md-1 col-sm-12">
+      <div class="col-sm-12">
         <img class="staff-image" src="<?php echo get_bloginfo( 'template_directory' );?>/images/about/About-Page_Isolated_Image.png" alt="">
       </div>
     </div>
-    <div class="row">
-      <div class="col-md-12">
+    <div class="row icon-row">
+      <div class="col-md-12 d-none d-md-block">
         <img class="icons-image" src="<?php echo get_bloginfo( 'template_directory' );?>/images/about/About_Icons_TEXT.png" alt="">
+      </div>
+      <div class="col-4 col-sm-4 d-sm-block d-md-none">
+        <img class="icon-mobile" src="<?php echo get_bloginfo( 'template_directory' );?>/images/about/ad_agency_logo.png" alt="">
+      </div>
+      <div class="col-4 col-sm-4 d-sm-block d-md-none">
+        <img class="icon-mobile" src="<?php echo get_bloginfo( 'template_directory' );?>/images/about/web_design_logo.png" alt="">
+      </div>
+      <div class="col-4 col-sm-4 d-sm-block d-md-none">
+        <img class="icon-mobile" src="<?php echo get_bloginfo( 'template_directory' );?>/images/about/health_design_logo.png" alt="">
       </div>
     </div>
   </div>
--- wp-content/themes/225/home.php
+++ wp-content/themes/225/home.php
@@ -6,16 +6,16 @@
   </div>
 </div>
 <div class="row" id="homeContent">
-  <hr>
+  <img class="gold-bar" src="<?php echo get_bloginfo( 'template_directory' );?>/images/homepage-header_2_gold_bar.jpg" alt="">
   <div class="col-sm-12 content-image">
     <div class="row">
-      <div class="col-sm-4 d-none d-lg-block">
+      <div class="col-sm-4 d-none d-sm-none d-md-none d-lg-block">
         <div class="content-text">
           <h1>A BEAUTIFUL <br> PARTNERSHIP</h1>
           <p>You deserve better advertising. Messages that go beyond the cliché. Images your audience relate to. Analytics you can understand. And results that get more than clicks and views, we drive calls and consultations.</p>
         </div>
       </div>
-      <div class="col-md-5 offset-md-7 col-sm-6 offset-sm-6 col-5 offset-7 d-lg-none">
+      <div class="col-md-5 offset-md-7 col-sm-6 offset-sm-6 col-7 offset-5 d-md-block d-lg-none">
         <div class="content-text-mobile">
           <h1>A BEAUTIFUL <br> PARTNERSHIP</h1>
           <p>You deserve better advertising. Messages that go beyond the cliché. Images your audience relate to. Analytics you can understand. And results that get more than clicks and views, we drive calls and consultations.</p>
